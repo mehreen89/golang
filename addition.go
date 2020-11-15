2b0c644e4a377d3b0b7eed70c19206504d1d6968
mehreen89 <mehreen89@gmail.com>
2020-11-14 20:08:40 -0800

    adding numbers

---
+++ addition.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main (){
+
+        fmt.Println("enter the first number")
+
+        var first int
+
+        fmt.Scanln(&first)
+
+        fmt.Println("enter the second number")
+       
+        var second int
+
+       fmt.Scanln(&second)
+
+       fmt.Println(first + second);      
+}
+
