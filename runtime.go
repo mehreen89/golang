6a246f564b3d7e7b3336187b1777b3212a509b35
mehreen89 <mehreen89@gmail.com>
2020-12-26 22:37:52 -0800

    bonus

---
+++ runtime.go
@@ -0,0 +1,22 @@
+package logging
+
+import (
+   "fmt"
+    "time"
+     "runtime"
+)
+
+var debug bool
+
+func Debug(b bool) {
+    debug = b
+}
+
+func Log(statement string) {
+    if !debug {
+        return
+    }
+
+    fmt.Printf("%s %s\n", time.Now().Format(time.RFC3339), statement)
+    fmt.Println(runtime.Caller(0))
+}
